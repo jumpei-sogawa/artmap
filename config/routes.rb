2011f4d401536605fae7085c172d6f2f4c3d5f02
jumpei-sogawa <jumpe123oon@gmail.com>
2018-12-16 05:44:49 +0900

    rootpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "arts#index"
   resources :arts
   resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
