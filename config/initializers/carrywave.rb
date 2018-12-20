396d1d53f0e6a288cbf183d4c6676c0f7a6c99f3
jumpei-sogawa <jumpe123oon@gmail.com>
2018-12-21 03:49:53 +0900

    ENV

--- config/initializers/carrywave.rb
+++ config/initializers/carrywave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1',
     host:                  's3.amazonaws.com/geek-rails-photo-sampler',             # optional, defaults to nil
     endpoint:              'https://s3.ap-northeast-1.amazonaws.com' # optional, defaults to nil
