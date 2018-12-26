ef8442a4503759b0a69ba5d9d0c8468f755ff1e0
jumpei-sogawa <jumpe123oon@gmail.com>
2018-12-26 11:56:03 +0900

    enable geocode to be made from link to google maps

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,6 +1,14 @@
 class Art < ApplicationRecord
   belongs_to :artist
   mount_uploader :image, ImageUploader
-  geocoded_by :address
-  after_validation :geocode
+  # geocoded_by :address
+  # after_validation :geocode
+
+  def geocode
+    location_link = self.location_link
+    s = location_link.split("@")
+    t = s[1].split(",")
+    self.latitude = t[0]
+    self.longitude = t[1]
+  end
 end
